c9904b9e0edcd53c0e76012a0e65bde40c7560c9
AISWARYA C B <aiswaryacb97@gmail.com>
2023-02-03 23:59:59 +0530

    Salesforce skills added

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 /* ============================== typing animation ============================ */
 var typed = new Typed(".typing",{
-    strings:["","Full Stack Developer","Web Designer","Web Developer","Cloud Engineer","DevOps Aspirant"],
+    strings:["","Salesforce Aspirant","Web Designer","Web Developer"],
     typeSpeed:100,
     BackSpeed:60,
     loop:true
